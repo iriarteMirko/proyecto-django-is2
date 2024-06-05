d5bc5c4cce7b1147ac2bea7a079760390b137368
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-06-05 00:45:04 -0500

    Update categoria de curso / templates

--- curso/admin.py
+++ curso/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Curso
 
 class CursoAdmin(admin.ModelAdmin):
-    list_display = ['nombre', 'descripcion', 'profesor']
+    list_display = ['nombre', 'descripcion', 'categoria', 'profesor', 'fecha_creacion', 'ultima_modificacion']
 
 admin.site.register(Curso, CursoAdmin)
