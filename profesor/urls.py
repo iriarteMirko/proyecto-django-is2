8c6fd9e075578d9150c54cded236c4eeebe3753d
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-06-08 22:21:43 -0500

    Update editar_perfil_profesor

--- profesor/urls.py
+++ profesor/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('signup/profesor/', views.signup_profesor, name='signup_profesor'),
     path('inicio/', views.inicio_profesor, name='inicio_profesor'),
     path('perfil/', views.mi_perfil, name='perfil_profesor'),
-    path('perfil/editar/', views.editar_perfil, name='editar_perfil'),
+    path('perfil/editar/', views.editar_perfil, name='editar_perfil_profesor'),
     path('cursos/', views.mis_cursos, name='cursos_profesor'),
     path('crear_curso/', views.crear_curso, name='crear_curso'),
     
