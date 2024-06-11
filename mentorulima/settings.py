7ee270728679b5b2ffe8e6e7ad6e03f52d544a6c
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-06-10 23:41:03 -0500

    Add 404.html / Update usuario views 404

--- mentorulima/settings.py
+++ mentorulima/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d&-x#zs#=8si0k#ug-qh*^$3x%9cv7yph4=_+til^t*j3^*g3w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
