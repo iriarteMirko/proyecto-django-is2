52d99f117bddbf7d056e49094526772123225b08
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-06-16 20:24:37 -0500

    Update curso/usuario

--- curso/models.py
+++ curso/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from profesor.models import Profesor
 
 class Curso(models.Model):
+    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=100, blank=False, null=False)
     descripcion = models.TextField(max_length=250, blank=False, null=False)
     categoria = models.CharField(max_length=100, blank=False, null=False)
-    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
+    calificacion = models.FloatField(default=0.0)
     fecha_creacion = models.DateField(auto_now_add=True)
     ultima_modificacion = models.DateField(auto_now=True)
     
