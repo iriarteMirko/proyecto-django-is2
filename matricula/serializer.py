91edce93b2456f4693ff8f991e3902929cec333c
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-06-17 22:03:22 -0500

    Add matricula: serializer, urls

---
+++ matricula/serializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Matricula
+
+class MatriculaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matricula
+        fields = '__all__'
