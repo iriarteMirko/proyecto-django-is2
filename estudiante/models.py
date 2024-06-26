1fb431428787fd0ba161904b29b73aa8a834766b
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-06-25 22:26:53 -0500

    Update class meta inscripcion

--- estudiante/models.py
+++ estudiante/models.py
@@ -2,8 +2,22 @@ from django.db import models
 from usuario.models import Usuario
 
 class Estudiante(models.Model):
+    CARRERA_OPCIONES = [
+        ('Arquitectura', 'Arquitectura'),
+        ('Administración', 'Administración'),
+        ('Contabilidad y Finanzas', 'Contabilidad y Finanzas'),
+        ('Economía', 'Economía'),
+        ('Marketing', 'Marketing'),
+        ('Negocios Internacionales', 'Negocios Internacionales'),
+        ('Comunicación', 'Comunicación'),
+        ('Derecho', 'Derecho'),
+        ('Ingeniería Civil', 'Ingeniería Civil'),
+        ('Ingeniería Industrial', 'Ingeniería Industrial'),
+        ('Ingeniería de Sistemas', 'Ingeniería de Sistemas'),
+        ('Psicología', 'Psicología'),
+    ]
     usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE)
-    carrera = models.CharField(max_length=100, blank=False, null=False)
+    carrera = models.CharField(max_length=100, choices=CARRERA_OPCIONES, blank=False, null=False)
     ciclo = models.PositiveIntegerField(blank=False, null=False, choices=[(i, i) for i in range(1, 13)])
     
     def __str__(self):
--- inscripcion/models.py
+++ inscripcion/models.py
@@ -8,5 +8,8 @@ class Inscripcion(models.Model):
     estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
     fecha_inscripcion = models.DateField(auto_now_add=True)
     
+    class Meta:
+        unique_together = ('curso', 'estudiante')
+    
     def __str__(self):
         return f'{self.estudiante.usuario.nombre} inscrito en {self.curso.nombre}'
