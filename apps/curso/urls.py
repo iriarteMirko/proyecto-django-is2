089d1c4e90ca71acf303725169b7baf3b4f9cc1b
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-07-01 08:09:57 -0500

    Update class active

--- apps/curso/urls.py
+++ apps/curso/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('lista-de-cursos/', views.mis_cursos, name='cursos_profesor'),
     path('<int:curso_id>/<str:curso_nombre>/detalle-curso/', views.detalle_curso, name='detalle_curso_profesor'),
-    path('<int:curso_id>/<str:curso_nombre>/informacion-curso/', views.informacion_curso, name='informacion_curso'),
     path('<int:curso_id>/editar-curso/', views.editar_curso, name='editar_curso'),
     path('<int:curso_id>/eliminar-curso/', views.eliminar_curso, name='eliminar_curso'),
+    path('<int:curso_id>/<str:curso_nombre>/informacion-curso/', views.detalle_curso, name='informacion_curso'),
     path('<int:curso_id>/<str:curso_nombre>/contenido-curso/', views.contenido_curso, name='contenido_curso'),
     path('<int:curso_id>/<str:curso_nombre>/asesoria-curso/', views.asesoria_curso, name='asesoria_curso'),
     
