096c7126d8c4c1a3f4f990d22a59da2e2d8ea0d3
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-07-06 16:24:26 -0500

    Update max lenght (500) para descripcion

--- apps/seccion/models.py
+++ apps/seccion/models.py
@@ -4,7 +4,7 @@ from apps.curso.models import Curso
 class Seccion(models.Model):
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=100, blank=False, null=False)
-    descripcion = models.TextField(max_length=250)
+    descripcion = models.TextField(max_length=500)
     
     class Meta:
         verbose_name = 'Sección'
