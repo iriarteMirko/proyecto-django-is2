528f34674eb8cf351232e1acca5630b823507542
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-07-08 08:09:06 -0500

    Update boton success

--- apps/curso/urls.py
+++ apps/curso/urls.py
@@ -9,7 +9,7 @@ router.register(r'cursos', views.CursoViewSet)
 urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('lista-de-cursos/', views.mis_cursos, name='lista_cursos'),
-    path('buscar_cursos/', views.buscar_cursos, name='buscar_cursos'),
+    path('buscar-cursos/', views.buscar_cursos, name='buscar_cursos'),
     path('<int:curso_id>/<slug:curso_slug>/informacion/', views.informacion_curso, name='informacion_curso'),
     path('<int:curso_id>/<slug:curso_slug>/contenido/', views.contenido_curso, name='contenido_curso'),
     path('<int:curso_id>/<slug:curso_slug>/asesoria/', views.asesoria_curso, name='asesoria_curso'),
--- apps/curso/views.py
+++ apps/curso/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.http import JsonResponse
-from django.db.models import Q
 from rest_framework import viewsets
 from .serializer import CursoSerializer
 from .form import CursoForm
