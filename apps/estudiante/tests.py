e174e6773da0806f0eb49d04045dad85643f78cb
Jonathan <jonathancallirgos@gmail.com>
2024-07-08 18:19:50 -0500

    prueba unitaria de estudiante

--- apps/estudiante/tests.py
+++ apps/estudiante/tests.py
@@ -1,3 +1,40 @@
+# apps/estudiante/tests.py
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from .models import Estudiante
 
-# Create your tests here.
+class EstudianteModelTests(TestCase):
+
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_user(codigo='20191627', email='20191627@aloe.ulima.edu.pe', nombre='Juan', apellidos='Gutiérrez', password='Elpapi12')
+    
+    def test_crear_estudiante(self):
+        estudiante = Estudiante.objects.create(
+            usuario=self.user,
+            carrera='Comunicación',
+            ciclo=9
+        )
+        self.assertEqual(estudiante.usuario.codigo, '20191627')
+        self.assertEqual(estudiante.carrera, 'Comunicación')
+        self.assertEqual(estudiante.ciclo, 9)
+
+    def test_estudiante_str(self):
+        estudiante = Estudiante.objects.create(
+            usuario=self.user,
+            carrera='Comunicación',
+            ciclo=9
+        )
+        self.assertEqual(str(estudiante), 'Juan Gutiérrez')
+
+    def test_estudiante_profesor_validacion(self):
+        self.user.es_profesor = True
+        self.user.save()
+        with self.assertRaises(ValidationError):
+            estudiante = Estudiante.objects.create(
+                usuario=self.user,
+                carrera='Comunicación',
+                ciclo=9
+            )
+            estudiante.clean()
