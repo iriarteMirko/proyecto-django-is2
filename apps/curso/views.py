a4b72fc3d21752b8695814c605ce3dba338601c9
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-07-08 12:46:11 -0500

    Update slugs/template inicio

--- apps/curso/views.py
+++ apps/curso/views.py
@@ -36,7 +36,7 @@ def buscar_cursos(request):
 
 @login_required
 def informacion_curso(request, curso_id, curso_slug):
-    curso = get_object_or_404(Curso, id=curso_id)
+    curso = get_object_or_404(Curso, id=curso_id, slug=curso_slug)
     return render(request, 'curso/informacion_curso.html', {'curso': curso, 'page': 'informacion'})
 
 @login_required
@@ -56,19 +56,19 @@ def eliminar_curso(request, curso_id):
 
 @login_required
 def contenido_curso(request, curso_id, curso_slug):
-    curso = get_object_or_404(Curso, id=curso_id)
+    curso = get_object_or_404(Curso, id=curso_id, slug=curso_slug)
     secciones = curso.seccion_set.all()
     return render(request, 'curso/contenido_curso.html', {'curso': curso, 'secciones': secciones, 'page': 'contenido'})
 
 @login_required
 def asesoria_curso(request, curso_id, curso_slug):
-    curso = get_object_or_404(Curso, id=curso_id)
+    curso = get_object_or_404(Curso, id=curso_id, slug=curso_slug)
     asesorias = curso.asesoria_set.all().order_by('fecha', 'hora_inicio')
     return render(request, 'curso/asesoria_curso.html', {'curso': curso, 'asesorias': asesorias, 'page': 'asesoria'})
 
 @login_required
 def estudiantes_curso(request, curso_id, curso_slug):
-    curso = get_object_or_404(Curso, id=curso_id)
+    curso = get_object_or_404(Curso, id=curso_id, slug=curso_slug)
     inscripciones = curso.inscripcion_set.all()
     estudiantes = [inscripcion.estudiante for inscripcion in inscripciones]
     return render(request, 'curso/estudiantes_curso.html', {'curso': curso, 'estudiantes': estudiantes, 'page': 'estudiantes'})
