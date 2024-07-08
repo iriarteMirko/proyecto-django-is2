f3632549029faed79e1ec5a4f075796889b25b80
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-07-08 18:43:09 -0500

    Update decorators inscripcion

--- apps/inscripcion/views.py
+++ apps/inscripcion/views.py
@@ -4,33 +4,50 @@ from rest_framework import viewsets
 from .serializer import InscripcionSerializer
 from .models import Inscripcion
 from apps.curso.models import Curso
+from apps.usuario.decorators import VistaBase, EstudianteDecorator, ProfesorDecorator
 
 class InscripcionViewSet(viewsets.ModelViewSet):
     serializer_class = InscripcionSerializer
     queryset = Inscripcion.objects.all()
 
+class InscripcionVista(VistaBase):
+    def procesar_solicitud(self, request, curso_id, *args, **kwargs):
+        curso = get_object_or_404(Curso, id=curso_id)
+        estudiante = request.user.estudiante
+        inscripcion = Inscripcion.objects.create(curso=curso, estudiante=estudiante)
+        if inscripcion:
+            return redirect('informacion_curso', curso_id=curso.id, curso_slug=curso.slug)
+        return redirect('buscar_cursos')
+
+class RetirarInscripcionVista(VistaBase):
+    def procesar_solicitud(self, request, curso_id, *args, **kwargs):
+        curso = get_object_or_404(Curso, id=curso_id)
+        estudiante = request.user.estudiante
+        inscripcion = get_object_or_404(Inscripcion, curso=curso, estudiante=estudiante)
+        inscripcion.delete()
+        return redirect('buscar_cursos')
+
+class RetirarEstudianteVista(VistaBase):
+    def procesar_solicitud(self, request, curso_id, estudiante_id, *args, **kwargs):
+        curso = get_object_or_404(Curso, id=curso_id)
+        inscripcion = get_object_or_404(Inscripcion, curso=curso, estudiante_id=estudiante_id)
+        inscripcion.delete()
+        return redirect('estudiantes_curso', curso_id=curso.id, curso_slug=curso.slug)
+
 @login_required
 def inscribir_curso(request, curso_id):
-    curso = get_object_or_404(Curso, id=curso_id)
-    estudiante = request.user.estudiante
-    inscripcion, created = Inscripcion.objects.get_or_create(curso=curso, estudiante=estudiante)
-    if inscripcion:
-        inscripcion.save()
-    if created:
-        return redirect('informacion_curso', curso_id=curso.id, curso_slug=curso.slug)
-    return redirect('buscar_cursos')
+    vista = InscripcionVista()
+    vistaDecorator = EstudianteDecorator(vista)
+    return vistaDecorator.procesar_solicitud(request, curso_id)
 
 @login_required
 def retirar_curso(request, curso_id):
-    curso = get_object_or_404(Curso, id=curso_id)
-    estudiante = request.user.estudiante
-    inscripcion = get_object_or_404(Inscripcion, curso=curso, estudiante=estudiante)
-    inscripcion.delete()
-    return redirect('buscar_cursos')
+    vista = RetirarInscripcionVista()
+    vistaDecorator = EstudianteDecorator(vista)
+    return vistaDecorator.procesar_solicitud(request, curso_id)
 
 @login_required
 def retirar_estudiante(request, curso_id, estudiante_id):
-    curso = get_object_or_404(Curso, id=curso_id)
-    inscripcion = get_object_or_404(Inscripcion, curso=curso, estudiante_id=estudiante_id)
-    inscripcion.delete()
-    return redirect('estudiantes_curso', curso_id=curso.id, curso_slug=curso.slug)
+    vista = RetirarEstudianteVista()
+    vistaDecorator = ProfesorDecorator(vista)
+    return vistaDecorator.procesar_solicitud(request, curso_id, estudiante_id)
