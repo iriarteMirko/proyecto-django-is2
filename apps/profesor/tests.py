82f166cfca7ef6f6191a3d15e907d3f0974b4ace
Jonathan <jonathancallirgos@gmail.com>
2024-07-08 18:27:45 -0500

    pruebas unitarias profesor

--- apps/profesor/tests.py
+++ apps/profesor/tests.py
@@ -1,3 +1,37 @@
+# apps/profesor/tests.py
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from .models import Profesor
 
-# Create your tests here.
+class ProfesorModelTests(TestCase):
+
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_user(
+            codigo='20191629', 
+            email='profesor@ulima.edu.pe', 
+            nombre='Luis', 
+            apellidos='Pérez', 
+            password='profesor123'
+        )
+
+    def test_crear_profesor(self):
+        profesor = Profesor.objects.create(
+            usuario=self.user,
+            profesion='Ingeniero',
+            centro_laboral='Universidad de Lima'
+        )
+        self.assertEqual(profesor.usuario.codigo, '20191629')
+        self.assertEqual(profesor.profesion, 'Ingeniero')
+
+    def test_profesor_clean_method(self):
+        self.user.es_estudiante = True
+        self.user.save()
+        with self.assertRaises(ValidationError):
+            profesor = Profesor.objects.create(
+                usuario=self.user,
+                profesion='Ingeniero',
+                centro_laboral='Universidad de Lima'
+            )
+            profesor.clean()
