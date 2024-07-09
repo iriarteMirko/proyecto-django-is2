53dc08581d882a91ae15ab47f51d486513994384
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-07-08 19:05:07 -0500

    Update validar decorators asesoria

--- apps/asesoria/views.py
+++ apps/asesoria/views.py
@@ -4,35 +4,45 @@ from rest_framework import viewsets
 from .serializer import AsesoriaSerializer
 from .models import Asesoria, validar_profesor, validar_asesoria
 from .form import AsesoriaForm
+from apps.usuario.decorators import VistaBase, ProfesorDecorator
 
 class AsesoriaViewSet(viewsets.ModelViewSet):
     serializer_class = AsesoriaSerializer
     queryset = Asesoria.objects.all()
 
+class EditarAsesoriaVista(VistaBase):
+    def procesar_solicitud(self, request, asesoria_id, *args, **kwargs):
+        asesoria = get_object_or_404(Asesoria, id=asesoria_id)
+        if request.method == 'POST':
+            error = validar_profesor(request, asesoria)
+            if error:
+                return render(request, 'base/404.html', {'error': error})
+            error = validar_asesoria(request)
+            if error:
+                asesorias = asesoria.curso.asesoria_set.all().order_by('fecha', 'hora_inicio')
+                return render(request, 'curso/asesoria_curso.html', {'curso': asesoria.curso, 'asesorias': asesorias, 'page': 'asesoria', 'error': error})
+            form = AsesoriaForm(request.POST, instance=asesoria)
+            if form.is_valid():
+                form.save()
+        return redirect('asesoria_curso', curso_id=asesoria.curso.id, curso_slug=asesoria.curso.slug)
+
+class EliminarAsesoriaVista(VistaBase):
+    def procesar_solicitud(self, request, asesoria_id, *args, **kwargs):
+        asesoria = get_object_or_404(Asesoria, id=asesoria_id)
+        error = validar_profesor(request, asesoria)
+        if error:
+            return render(request, 'base/404.html', {'error': error})
+        asesoria.delete()
+        return redirect('asesoria_curso', curso_id=asesoria.curso.id, curso_slug=asesoria.curso.slug)
+
 @login_required
 def editar_asesoria(request, asesoria_id):
-    asesoria = get_object_or_404(Asesoria, id=asesoria_id)
-    error = validar_profesor(request, asesoria)
-    if error:
-        return render(request, 'base/404.html', {'error': error})
-    curso = asesoria.curso
-    if request.method == 'POST':
-        error = validar_asesoria(request)
-        if error:
-            asesorias = curso.asesoria_set.all().order_by('fecha', 'hora_inicio')
-            return render(request, 'curso/asesoria_curso.html', {'curso': curso, 'asesorias': asesorias, 'page': 'asesoria', 'error': error})
-        form = AsesoriaForm(request.POST, instance=asesoria)
-        if form.is_valid():
-            form.save()
-            return redirect('asesoria_curso', curso_id=curso.id, curso_slug=curso.slug)
-    asesorias = curso.asesoria_set.all().order_by('fecha', 'hora_inicio')
-    return render(request, 'curso/asesoria_curso.html', {'curso': curso, 'asesorias': asesorias, 'page': 'asesoria'})
+    vista = EditarAsesoriaVista()
+    vistaDecorada = ProfesorDecorator(vista)
+    return vistaDecorada.procesar_solicitud(request, asesoria_id)
 
 @login_required
 def eliminar_asesoria(request, asesoria_id):
-    asesoria = get_object_or_404(Asesoria, id=asesoria_id)
-    error = validar_profesor(request, asesoria)
-    if error:
-        return render(request, 'base/404.html', {'error': error})
-    asesoria.delete()
-    return redirect('asesoria_curso', curso_id=asesoria.curso.id, curso_slug=asesoria.curso.slug)
+    vista = EliminarAsesoriaVista()
+    vistaDecorada = ProfesorDecorator(vista)
+    return vistaDecorada.procesar_solicitud(request, asesoria_id)
--- apps/inscripcion/views.py
+++ apps/inscripcion/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from rest_framework import viewsets
 from .serializer import InscripcionSerializer
