41403e85bd6937d5acc297ff43e6a74c53c32dec
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-07-08 19:10:01 -0500

    Update decorator para secciones

--- apps/seccion/views.py
+++ apps/seccion/views.py
@@ -4,25 +4,38 @@ from rest_framework import viewsets
 from .serializer import SeccionSerializer
 from .models import Seccion
 from .form import SeccionForm
+from apps.usuario.decorators import ProfesorDecorator
 
 class SeccionViewSet(viewsets.ModelViewSet):
     serializer_class = SeccionSerializer
     queryset = Seccion.objects.all()
 
+class EditarSeccionDecorator(ProfesorDecorator):
+    def procesar_solicitud(self, request, seccion_id, *args, **kwargs):
+        seccion = get_object_or_404(Seccion, id=seccion_id)
+        if request.method == 'POST':
+            form = SeccionForm(request.POST, instance=seccion)
+            if form.is_valid():
+                form.save()
+                return redirect('contenido_curso', curso_id=seccion.curso.id, curso_slug=seccion.curso.slug)
+        curso = seccion.curso
+        secciones = curso.seccion_set.all()
+        return render(request, 'curso/contenido_curso.html', {'curso': curso, 'secciones': secciones, 'page': 'contenido'})
+
+class EliminarSeccionDecorator(ProfesorDecorator):
+    def procesar_solicitud(self, request, seccion_id, *args, **kwargs):
+        seccion = get_object_or_404(Seccion, id=seccion_id)
+        seccion.delete()
+        return redirect('contenido_curso', curso_id=seccion.curso.id, curso_slug=seccion.curso.slug)
+
 @login_required
 def editar_seccion(request, seccion_id):
-    seccion = get_object_or_404(Seccion, id=seccion_id)
-    if request.method == 'POST':
-        form = SeccionForm(request.POST, instance=seccion)
-        if form.is_valid():
-            form.save()
-            return redirect('contenido_curso', curso_id=seccion.curso.id, curso_slug=seccion.curso.slug)
-    curso = seccion.curso
-    secciones = curso.seccion_set.all()
-    return render(request, 'curso/contenido_curso.html', {'curso': curso, 'secciones': secciones, 'page': 'contenido'})
+    vista = EditarSeccionDecorator()
+    vistaDecorada = ProfesorDecorator(vista)
+    return vistaDecorada.procesar_solicitud(request, seccion_id)
 
 @login_required
 def eliminar_seccion(request, seccion_id):
-    seccion = get_object_or_404(Seccion, id=seccion_id)
-    seccion.delete()
-    return redirect('contenido_curso', curso_id=seccion.curso.id, curso_slug=seccion.curso.slug)
+    vista = EliminarSeccionDecorator()
+    vistaDecorada = ProfesorDecorator(vista)
+    return vistaDecorada.procesar_solicitud(request, seccion_id)
