360a6e593464576a86bc1c71283f41de09c138e5
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-07-10 19:33:21 -0500

    Update test material

--- apps/material/tests.py
+++ apps/material/tests.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from apps.profesor.models import Profesor
 from apps.curso.models import Curso
 from apps.seccion.models import Seccion
-from .models import Material
+from apps.material.models import Material
 import tempfile
 from django.core.files.uploadedfile import SimpleUploadedFile
 
@@ -12,14 +12,26 @@ User = get_user_model()
 
 class MaterialTestCase(TestCase):
     def setUp(self):
-        # Set up a temporary file to act as the material file
+        # Creación material temporal para pruebas
         self.temp_file = tempfile.NamedTemporaryFile(suffix=".txt")
         self.temp_file.write(b"Test file content")
         self.temp_file.seek(0)
-        
-        # Create a test user, professor, course, and section
-        self.user = User.objects.create_user(username='testuser', password='12345', es_profesor=True)
-        self.profesor = Profesor.objects.create(usuario=self.user)
+        # Creación de usuario
+        self.user = User.objects.create_user(
+            codigo='testcode',
+            email='testuser@example.com',
+            nombre='Test',
+            apellidos='User',
+            password='12345',
+            es_profesor=True
+        )
+        # Creación de profesor
+        self.profesor = Profesor.objects.create(
+            usuario=self.user,
+            profesion='Ingeniero de Software',
+            centro_laboral='Universidad de Lima'
+        )
+        # Creación de curso
         self.curso = Curso.objects.create(
             nombre='Test Curso',
             descripcion='Descripción del curso de prueba',
@@ -28,35 +40,33 @@ class MaterialTestCase(TestCase):
             profesor=self.profesor,
             slug='test-curso'
         )
+        # Creación de sección
         self.seccion = Seccion.objects.create(
             curso=self.curso,
             nombre='Test Seccion',
             descripcion='Descripción de la sección de prueba'
         )
+        # Iniciar sesión
         self.client = Client()
-        self.client.login(username='testuser', password='12345')
-
+        self.client.login(codigo='testcode', password='12345')
+    
     def test_agregar_material(self):
-        # Create a simple uploaded file for testing
+        # Creación de un archivo simple para pruebas
         material_file = SimpleUploadedFile(self.temp_file.name, self.temp_file.read(), content_type='text/plain')
-
         response = self.client.post(reverse('agregar_material', kwargs={'seccion_id': self.seccion.id}), {
             'nombre': 'Test Material',
             'archivo': material_file
         })
-
-        self.assertEqual(response.status_code, 302)  # Redirection after successful creation
+        self.assertEqual(response.status_code, 302)  # Redirección después de la creación exitosa
         self.assertTrue(Material.objects.filter(nombre='Test Material').exists())
-
+    
     def test_eliminar_material(self):
-        # Create a material instance
+        # Creación de un material para eliminar
         material = Material.objects.create(
             seccion=self.seccion,
             nombre='Material para Eliminar',
             archivo=SimpleUploadedFile(self.temp_file.name, self.temp_file.read(), content_type='text/plain')
         )
-
         response = self.client.post(reverse('eliminar_material', kwargs={'material_id': material.id}))
-
-        self.assertEqual(response.status_code, 302)  # Redirection after successful deletion
+        self.assertEqual(response.status_code, 302)  # Redirección después de la eliminación exitosa
         self.assertFalse(Material.objects.filter(nombre='Material para Eliminar').exists())
